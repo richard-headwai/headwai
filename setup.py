76f1df07ca15d15d357331a4521c8ec1a6fb4251
richard-headwai <163117794+richard-headwai@users.noreply.github.com>
2024-03-12 13:22:43 +0100

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='headwai',
-    version='0.1.0',
+    version='0.1.1',
     author='Richard',
     author_email='richard.deetlefs@headwai.org',
     description='A package for evaluating text correctness.',

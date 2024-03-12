ecb9c46f25aef9ace322325500cd3d7566eb5861
richard-headwai <163117794+richard-headwai@users.noreply.github.com>
2024-03-12 13:03:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
 
+# Ensure 'README.md' is in the same directory as 'setup.py'
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='headwai',
     version='0.1.0',
-    author='richard',
+    author='Richard',
     author_email='richard.deetlefs@headwai.org',
     description='A package for evaluating text correctness.',
-    long_description=open('README.md').read(),
+    long_description=long_description,  
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/headwai',
+    url='https://github.com/richard-headwai/headwai',  
     packages=find_packages(),
     install_requires=[
-        'asyncio',
         'websockets',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License', 
         'Operating System :: OS Independent',
     ],
+    license='MIT',
     python_requires='>=3.10',
 )
